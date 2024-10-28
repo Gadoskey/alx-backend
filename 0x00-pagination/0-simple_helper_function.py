bcaade98a9085f067512370b6afcd5efcd67ba79
Gadoskey <gadoski321@gmail.com>
2024-10-28 21:04:50 +0100

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,13 +7,14 @@ File: 0-simple_helper_function.py
 Description: A Python function named index_range that takes
 two integer arguments page and page_size. The function returns a tuple
 of size two containing a start index and an end index corresponding to the
-range of indexes to return in a list for those particular pagination parameters.
+range of indexes to return in a list.
 """
 
 
 def index_range(page, page_size):
-  """ function index_range that takes two integer arguments page and page_size
+  """function index_range that takes two integer arguments page and page_size
   Return: the start_page and page_size
   """
   start_page = (page - 1) * page_size
-  return {start_page, page_size}
+  end_page = start_page + page_size
+  return (start_page, end_page)
