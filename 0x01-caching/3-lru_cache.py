ffdefa6e9176b128e3a7c8caf313f2de17b00a63
Gadoskey <gadoski321@gmail.com>
2024-11-03 11:29:42 +0100

    debuging

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -62,4 +62,6 @@ class LRUCache(BaseCaching):
         """
         if key is None:
             return None
+        self.order.remove(key)
+        self.order.append(key)
         return self.cache_data.get(key)
